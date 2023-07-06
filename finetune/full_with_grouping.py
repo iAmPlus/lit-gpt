c5a19d6bb545ee5fa2d2cc9bb5e0c1cff21ec395
manojpreveen <manojpreveen@gmail.com>
2023-07-06 13:24:28 +0000

    Updates

--- finetune/full_with_grouping.py
+++ finetune/full_with_grouping.py
@@ -20,9 +20,9 @@ from lit_gpt.tokenizer import Tokenizer
 from lit_gpt.utils import lazy_load, check_valid_checkpoint_dir, step_csv_logger, chunked_cross_entropy
 from lit_gpt.speed_monitor import SpeedMonitor, measure_flops, estimate_flops
 
-eval_interval = 600
-save_interval = 1000
-eval_iters = 100
+eval_interval = 3000
+save_interval = 3000
+eval_iters = 300
 log_interval = 1
 devices = 8
 # change this value to force a maximum sequence length
@@ -30,12 +30,12 @@ override_max_seq_length = 2020
 
 # Hyperparameters
 learning_rate = 2e-5
-batch_size = 16 / devices
-micro_batch_size = 2
+batch_size = 64 / devices
+micro_batch_size = 4
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
-epoch_size = 1400  # train dataset size
-num_epochs = 1
+epoch_size = 161741  # train dataset size
+num_epochs = 4
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
 weight_decay = 0.02
 warmup_steps = 2 * (epoch_size // micro_batch_size) // devices // gradient_accumulation_iters # 2 epochs
--- scripts/prepare_it_with_grouping.py
+++ scripts/prepare_it_with_grouping.py
@@ -79,7 +79,7 @@ def prepare(
         raw_datasets["train"] = load_dataset(
             extension,
             data_files=data_files,
-            split=f"train[:{test_split_percentage}%]",
+            split=f"train[:]",
         )
 
     print("Loading tokenizer...")
@@ -111,12 +111,6 @@ def prepare(
         end_token_id = tokenizer.encode(END_TOKEN).item()
         pad_token_id = tokenizer.eos_id
         next_line_token_id = tokenizer.encode("\n").item()
-
-        print("user_token_ids : ", user_token_ids)
-        print("assistant_token_ids : ", assistant_token_ids)
-        print("end_token : ", end_token_id)
-        print("pad_token : ", pad_token_id)
-        print("next_line_token_id : ", next_line_token_id)
 
         keys_list = list(examples.keys())
 
@@ -207,11 +201,6 @@ def download_if_missing(file_path: Path, file_url: str):
         return
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(requests.get(file_url).text)
-
-def generate_prompt_task(example):
-    return(
-        f"<human>: You are a helpful assistant who can break incoming main task into an immediate sub task to complete the main task. You should use the observations and actions made till that point to come up with the next immediate sub task. And the next immediate sub task must fall into one of the domains below.\n\nDomain 1 : movies_showtimes\nAll the tasks related to searching for movies, movie’s showtimes, etc falls in this domain. \n\nDomain 2 : movies_buy_tickets\nAll the tasks related to booking a movie ticket, searching for the availability of movie tickets, etc falls in this domain. \n\nDomain 3 : calendar_show_event\nAll the tasks related to enquiring about a person's availablility, information about someone’s free times, etc falls in this domain. \n\nDomain 4 : END\nIf the task is already completed with the previous sub tasks as given in the observation and actions made so far, then it should belong to this domain.\n\nGiven a main task and the previous observations and actions made so far, you have to come up with the next immediate sub task in order to complete the main task such that the sub task belongs to one of the domains above. If there are no more sub tasks required to complete the main task, then just reply with '(Domain: End)'. The subtask should always end with '###'.\n\nMainTask: {example['input']}\nObservations and actions made Till now : \n\n <bot>: "
-    )
 
 def generate_prompt(example):
     return (       
